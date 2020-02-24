c0a5e6b84e5e61ec2aa00a07d54dca125ccfabd1
Ashot-PC\Ashot <ashot11997@mail.ru>
2020-02-24 16:58:28 +0400

    created loading system

---
+++ API Project/Assets/Scripts/LoadMountains.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LoadMountains : MonoBehaviour
+{
+
+	private string JsonUrl = "https://drive.google.com/uc?id=1i31oF4XbEfW39RX-dQSUxTi9cLbh31my";
+
+	public IEnumerator Load(Action<PlacesList, int> callback)
+	{
+		Debug.Log("Loading Categories");
+		WWW www = new WWW(JsonUrl);
+		yield return www;
+		if (www.error == null)
+		{
+			ProcessJson(www.text, callback);
+		}
+
+
+	}
+
+	void ProcessJson(string data, Action<PlacesList, int> callback)
+	{
+		Tenplate dest = JsonUtility.FromJson<Tenplate>(data);
+
+		foreach (PlacesList x in dest.List)
+		{
+			callback.Invoke(x, dest.List.Count);
+		}
+	}
+}
---
+++ API Project/Assets/Scripts/Template.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[Serializable]
+public class Tenplate
+{
+	public List<PlacesList> List;
+}
+
+[Serializable]
+public class PlacesList
+{
+	public string Name;
+	public string ThumbnailUrl;
+	public string Description;
+	public string Address;
+	public string Category;
+	public string Distance;
+	public string LongDescription;
+
+}
