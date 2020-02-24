c4baaa19ce4fc046652ede3b8ce16d1d93b8797b
Ashot-PC\Ashot <ashot11997@mail.ru>
2020-02-24 17:05:47 +0400

    created calling script

---
+++ API Project/Assets/Scripts/APISytem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class APISytem : MonoBehaviour
+{
+	public Button CallBtn;
+
+	public LoadMountains MountainsLoader;
+
+	private void Awake()
+	{
+		CallBtn.onClick.AddListener(CallMountains);
+	}
+
+	void CallMountains() {
+		StartCoroutine(MountainsLoader.Load((mountain, count) => {
+
+		}));
+	}
+}
