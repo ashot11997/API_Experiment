0e5cd2a5616293110da2030cc245f1a05372a07b
Ashot-PC\Ashot <ashot11997@mail.ru>
2020-02-24 17:15:03 +0400

    created scrollview

---
+++ API Project/Assets/Scripts/MountainsPrefab.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MountainsPrefab : MonoBehaviour
+{
+	public Text Title;
+	public Text Category;
+	public Text Description;
+	public RawImage Thumbnail;
+
+	public void Setup(PlacesList place, string category){
+		Title.text = char.ToUpper(place.Name[0]) + place.Name.Substring(1);
+		Description.text = place.Description;
+		Category.text = category;
+		StartCoroutine(LoadImage(place.ThumbnailUrl, texture =>{
+			Thumbnail.texture = texture;
+		}));
+	}
+
+	IEnumerator LoadImage(string url, Action<Texture> texture) {
+		WWW www = new WWW(url);
+		yield return www;
+		texture.Invoke(www.texture);
+	}
+
+}
